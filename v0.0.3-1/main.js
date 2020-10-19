9e5da0078e97fe9b8187589d27dd5f1ef3fca3f4
basementdwellers69 <68178459+basementdwellers69@users.noreply.github.com>
2020-10-19 18:15:35 +0800

    v. 0.1.0 - main.js

--- v0.0.3-1/main.js
+++ v0.0.3-1/main.js
@@ -4,8 +4,9 @@ import {createWireframeRenderer} from './render.js';
 
 
 const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvasWindow = document.querySelector('#canvas');
+canvas.width = canvasWindow.clientWidth;
+canvas.height = canvasWindow.clientHeight;
 
 const mesh1 = createMesh(cube);
 mesh1.color = '#000';
@@ -13,6 +14,7 @@ mesh1.color = '#000';
 const scene = [mesh1];
 let meshCtrld = 0;
 
+///FILE INPUT HANDLER
 window.addEventListener('load', function() {
   var upload = document.getElementById('fileInput');
   
@@ -39,6 +41,137 @@ window.addEventListener('load', function() {
     });
   }
 });
+
+
+/// POLYGON LIST HANDLER
+var liVal = 0;
+var input = document.querySelectorAll('input');
+var polyListButton = document.getElementById("enter");
+var ul = document.querySelector("ul");
+var list = document.getElementsByTagName("li");
+var trash = document.getElementsByClassName("delete");
+var btndelete = document.getElementById("trash");
+
+//For removing items with delete button
+Array.prototype.slice.call(trash).forEach(function(item) {
+  item.addEventListener("click", function(e) {
+    console.log("element deleted");
+    e.target.parentNode.remove();
+  });
+})
+
+//loop for to strikeout the list 
+for (var i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", strikeout);
+}
+
+//toggle between classlist
+function strikeout() {
+  this.classList.toggle("done");
+}
+
+//check the length of the string entered
+function inputlength(obj) {return obj.value.length;}
+
+///json formatter
+function prettyPrintArray(json) {
+    let output = json.map(x=>JSON.stringify(x,null,1));
+    return JSON.stringify(output, null, '<br>').replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\"\&])/g,'').replace(/n/g,'');
+}
+
+//collect data that is inserted 
+function addli(obj) {
+    if(ul.children.length === 0){
+        var li = document.createElement("li");
+        var btn = document.createElement("button");
+        btn.className = "delete";
+        btn.innerHTML = "X";
+        btn.addEventListener("click", (e) => { ifDelLi(e); });
+        li.addEventListener("click", strikeout);
+        li.innerHTML = prettyPrintArray(obj);
+        li.value = 0;
+        liVal++;
+        li.appendChild(btn);
+        ul.appendChild(li);
+        for(let i = 1; i<=9; i++){
+            input[i].value = "";
+        }
+    }
+    else
+    {
+        var li = document.createElement("li");
+        var btn = document.createElement("button");
+        btn.className = "delete";
+        btn.innerHTML = "X";
+        btn.addEventListener("click", (e) => { ifDelLi(e); });
+        li.addEventListener("click", strikeout);
+        li.innerHTML = prettyPrintArray(obj);
+        li.value = liVal;
+        liVal++;
+        li.appendChild(btn);
+        ul.appendChild(li);
+        for(let i = 1; i<=9; i++){
+            input[i].value = "";
+        }
+    }
+    
+}
+
+function ifDelLi(e){
+    let val = parseInt(e.target.parentNode.value);
+    if(val === null || val === 0){
+        scene[meshCtrld].polygons.splice(0,1);
+        e.target.parentNode.remove();
+    }else{
+        scene[meshCtrld].polygons.splice(val,1);
+        e.target.parentNode.remove();
+    }
+    updateLi();
+    render(scene, camera);
+}
+function resetList(){
+    liVal=0;
+    temp=0;
+    delCount=0;
+    ul.innerHTML = '';
+    scene[meshCtrld].polygons.map(addli);
+}
+
+function updateLi(){
+    for(let i = 0; i <= ul.children.length-1; i++){
+        ul.children[i].value = i;
+    }
+}
+
+//this will add a new list item after click 
+function addListAfterClick() {
+  if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0) {
+      let temp = [new Vec(input[1].value,input[2].value,input[3].value),
+                  new Vec(input[4].value,input[5].value,input[6].value),
+                  new Vec(input[7].value,input[8].value,input[9].value)];
+    scene[meshCtrld].polygons.push(temp);
+    addli(temp);
+    render(scene, camera);
+  }
+}
+
+//this will add a new list item with keypress
+function addListKeyPress(event) {
+  if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0 || event.which === 13){
+    let temp = [new Vec(input[1].value,input[2].value,input[3].value),
+                  new Vec(input[4].value,input[5].value,input[6].value),
+                  new Vec(input[7].value,input[8].value,input[9].value)];
+    scene[meshCtrld].polygons.push(temp);
+    addli(temp);
+    render(scene, camera);
+  }
+}
+
+//this will check for the event/keypress and create new list item
+input[1].addEventListener("keypress", addListKeyPress);
+
+//this will check for a click event and create new list item
+polyListButton.addEventListener("click", addListAfterClick);
 
 
 class Camera {
@@ -83,7 +216,8 @@ function animate(time) {
     requestAnimationFrame(animate);
 }
 
-console.log(mesh1);
+scene[meshCtrld].polygons.map(addli);
+
 
 ///CHOOSE MESH BUTTON CONTROLS
 const chooseMeshButton = document.querySelector('button');
@@ -99,11 +233,13 @@ chooseMeshButton.onclick = function(){
         chooseMeshButton.value = value;
         chooseMeshButton.innerHTML = value;
         meshCtrld = value;
+        resetList();
     }else{
         value = 0;
         chooseMeshButton.value = value;
         chooseMeshButton.innerHTML = 0;
         meshCtrld = 0;
+        resetList();
     }
 }
 
