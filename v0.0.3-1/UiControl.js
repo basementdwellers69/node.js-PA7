cf334ec7ecde7529ecc050df0a9e66786110b3f4
basementdwellers69 <68178459+basementdwellers69@users.noreply.github.com>
2020-10-22 01:05:47 +0800

    V.0.1.2 UI-control.js update

--- v0.0.3-1/UiControl.js
+++ v0.0.3-1/UiControl.js
@@ -1,15 +1,46 @@
 import{scene, meshCtrld, render, camera, onClick} from './main.js';
-import {createMesh, Vec} from './mesh.js';
+import {createMesh, Vec, rotate, offset} from './mesh.js';
+import {offsetToCenter} from './render.js';
+
+/// CONTROLING THE LOAD OBJECT
+export function fileHandler(e) {
+  var upload = document.getElementById('fileInput');
+  
+  // Make sure the DOM element exists
+  if (upload) 
+  {
+    upload.addEventListener('change', function() {
+      // Make sure a file was selected
+      if (upload.files.length > 0) 
+      {
+        var reader = new FileReader(); // File reader to read the file 
+        
+        // This event listener will happen when the reader has read the file
+        reader.addEventListener('load', function() {
+          var result = JSON.parse(reader.result); // Parse the result into an object      
+                var model = createMesh(result.obj);
+                model.color = "blue";
+                scene.push(model);
+                render(scene, camera);
+        });
+        
+        reader.readAsText(upload.files[0]); // Read the uploaded file
+      }
+    });
+  }
+}
 
 
 /// POLYGON LIST HANDLER
-var liVal = 0;
-export var input = document.querySelectorAll('input');
-export var polyListButton = document.getElementById("enter");
-export var ul = document.querySelector("ul");
-export var list = document.getElementsByTagName("li");
-export var trash = document.getElementsByClassName("delete");
-export var btndelete = document.getElementById("trash");
+let editPointMode = false;
+let liVal = 0;
+let choosePoly = 0;
+export let input = document.querySelectorAll('input');
+export let polyListButton = document.getElementById("enter");
+export let ul = document.querySelector("ul");
+export let list = document.getElementsByTagName("li");
+export let trash = document.getElementsByClassName("delete");
+export let btndelete = document.getElementById("trash");
 
 //For removing items with delete button
 Array.prototype.slice.call(trash).forEach(function(item) {
@@ -19,14 +50,21 @@ Array.prototype.slice.call(trash).forEach(function(item) {
   });
 })
 
-//loop for to strikeout the list 
-for (var i = 0; i < list.length; i++) {
-  list[i].addEventListener("click", strikeout);
-}
 
 //toggle between classlist
-function strikeout() {
-  this.classList.toggle("done");
+function choosePoints() {
+    if(editPointMode === false){
+        editPointMode = true;
+        updateInputOnClick(this);
+        polyListButton.innerHTML = "Update Polygon";
+    }else{
+        editPointMode = false;
+        for(let i = 1; i<=9; i++){
+            input[i].value = "";
+            polyListButton.innerHTML = "Enter";
+        }
+    }
+    choosePoly = this.value;
 }
 
 //check the length of the string entered
@@ -35,7 +73,7 @@ function inputlength(obj) {return obj.value.length;}
 ///json formatter
 function prettyPrintArray(json) {
     let output = json.map(x=>JSON.stringify(x,null,1));
-    return JSON.stringify(output, null, '<br>').replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\"\&])/g,'').replace(/n/g,'');
+    return JSON.stringify(output, null, '<p>').replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\"\&])/g,'').replace(/n/g,'').replace("<br>",'');
 }
 
 //collect data that is inserted 
@@ -46,7 +84,7 @@ export function addli(obj) {
         btn.className = "delete";
         btn.innerHTML = "X";
         btn.addEventListener("click", (e) => { ifDelLi(e); });
-        li.addEventListener("click", strikeout);
+        li.addEventListener("click", choosePoints);
         li.innerHTML = prettyPrintArray(obj);
         li.value = 0;
         li.onmouseover= function(){hover(this)};
@@ -65,7 +103,7 @@ export function addli(obj) {
         btn.className = "delete";
         btn.innerHTML = "X";
         btn.addEventListener("click", (e) => { ifDelLi(e); });
-        li.addEventListener("click", strikeout);
+        li.addEventListener("click", choosePoints);
         li.innerHTML = prettyPrintArray(obj);
         li.value = liVal;
         li.onmouseover= function(){hover(this)};
@@ -106,121 +144,147 @@ function updateLi(){
 
 //this will add a new list item after click 
 export function addListAfterClick() {
-  if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0) {
-      let temp = [new Vec(input[1].value,input[2].value,input[3].value),
-                  new Vec(input[4].value,input[5].value,input[6].value),
-                  new Vec(input[7].value,input[8].value,input[9].value)];
-    scene[meshCtrld].polygons.push(temp);
-    addli(temp);
-    render(scene, camera);
-  }
+    if(editPointMode === true){
+        if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0) {
+          let temp = [new Vec(input[1].value,input[2].value,input[3].value),
+                      new Vec(input[4].value,input[5].value,input[6].value),
+                      new Vec(input[7].value,input[8].value,input[9].value)];
+            scene[meshCtrld].polygons[choosePoly] = temp;
+            resetList();
+            render(scene, camera);
+    }else{
+        if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0) {
+          let temp = [new Vec(input[1].value,input[2].value,input[3].value),
+                      new Vec(input[4].value,input[5].value,input[6].value),
+                      new Vec(input[7].value,input[8].value,input[9].value)];
+            scene[meshCtrld].polygons.push(temp);
+            addli(temp);
+            render(scene, camera);
+        }
+    }
+}
 }
 
-//this will add a new list item with keypress
-export function addListKeyPress(event) {
-  if (inputlength(input[1]) > 0 && inputlength(input[2]) > 0 && inputlength(input[3]) > 0 && inputlength(input[4]) > 0 && inputlength(input[5]) > 0 && inputlength(input[6]) > 0 && inputlength(input[7]) > 0 && inputlength(input[8]) > 0 && inputlength(input[9]) > 0 || event.which === 13){
-    let temp = [new Vec(input[1].value,input[2].value,input[3].value),
-                  new Vec(input[4].value,input[5].value,input[6].value),
-                  new Vec(input[7].value,input[8].value,input[9].value)];
-    scene[meshCtrld].polygons.push(temp);
-    addli(temp);
-    render(scene, camera);
-  }
+function updateInputOnClick(element){
+    let temp = scene[meshCtrld].polygons[element.value];
+    input[1].value = temp[0].x;
+    input[2].value = temp[0].y;
+    input[3].value = temp[0].z;
+    input[4].value = temp[1].x;
+    input[5].value = temp[1].y;
+    input[6].value = temp[1].z;
+    input[7].value = temp[2].x;
+    input[8].value = temp[2].y;
+    input[9].value = temp[2].z;
+    
 }
 
 ///CHANGE POLYGON COLOR ON <LI> HOVER///
 function hover(element) {
-    element.style.backgroundColor = "#e6e6e6";
-    scene[meshCtrld].polygons[element.value].color="red";
+    switch(editPointMode){
+        case false:
+            scene[meshCtrld].polygons[element.value].color="red";
+            element.style.backgroundColor = "#e6e6e6";
+            break;
+        case true:
+            scene[meshCtrld].polygons[element.value].color="red";
+            (element.value === choosePoly) ? element.style.backgroundColor = "#e6e6e6" : element.style.backgroundColor = "white";
+            break;
+    }
     render(scene,camera);
 }
+
 function hoverOff(element) {
-    element.style.backgroundColor = "white";
-    delete scene[meshCtrld].polygons[element.value].color;
+    switch(editPointMode){
+        case false:
+            element.style.backgroundColor = "white";
+            delete scene[meshCtrld].polygons[element.value].color;
+            break;
+        case true:
+            if (element.value === choosePoly){
+                element.style.backgroundColor = "#e6e6e6";
+                scene[meshCtrld].polygons[element.value].color="red";
+            }else{
+                element.style.backgroundColor = "white";
+                delete scene[meshCtrld].polygons[element.value].color;
+            }
+            break;
+    }
     render(scene,camera);
+
 }
+
 
 //KEYBOARD CONTROL (ZOOM MOVE AND SHIT)
-export function press(e){
-        if(onClick===true){
-            if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */){
-                scene[meshCtrld].rotation.x += 0.01;
-                render(scene, camera);
-            }
-            else if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-                scene[meshCtrld].rotation.y -= 0.01;
-                render(scene, camera);
-            }
-            else if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-                scene[meshCtrld].rotation.x -= 0.01;
-                render(scene, camera);
-            }
-            else if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
-                scene[meshCtrld].rotation.y += 0.01;
-                render(scene, camera);
-            }
-            else if(e.keyCode === 90 /* z */){
-                scene[meshCtrld].position.z -= 1;
-                render(scene, camera);
-            }
-            else if(e.keyCode === 88 /* z */){
-                scene[meshCtrld].position.z += 1;
-                render(scene, camera);
-            }
-        }
-        else if(onClick===false){
-            if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */){
-              scene[meshCtrld].position.y -= 1;
-              render(scene, camera);
-            }
-            else if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-              scene[meshCtrld].position.x += 1;
-              render(scene, camera);
-            }
-            else if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-              scene[meshCtrld].position.y += 1;
-            render(scene, camera);
-            }
-            else if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */){
-              scene[meshCtrld].position.x -= 1;
-              render(scene, camera);
-            }
-            else if(e.keyCode === 90 /* z */){
-                scene[meshCtrld].position.z -= 1;
-                render(scene, camera);
-            }
-            else if(e.keyCode === 88 /* x */){
-                scene[meshCtrld].position.z += 1;
-                render(scene, camera);
-            }
-        }
-     
-    }
 
-/// CONTROLING THE LOAD OBJECT
-export function fileHandler(e) {
-  var upload = document.getElementById('fileInput');
-  
-  // Make sure the DOM element exists
-  if (upload) 
-  {
-    upload.addEventListener('change', function() {
-      // Make sure a file was selected
-      if (upload.files.length > 0) 
-      {
-        var reader = new FileReader(); // File reader to read the file 
-        
-        // This event listener will happen when the reader has read the file
-        reader.addEventListener('load', function() {
-          var result = JSON.parse(reader.result); // Parse the result into an object      
-                var model = createMesh(result.obj);
-                model.color = "blue";
-                scene.push(model);
-                render(scene, camera);
-        });
-        
-        reader.readAsText(upload.files[0]); // Read the uploaded file
-      }
-    });
-  }
+export function press(e){
+    switch(onClick){
+        case true:
+            switch(e.keyCode){
+                case 38:
+                case 87:
+                    scene[meshCtrld].rotation.x += 0.01;
+                    render(scene, camera);
+                    break;
+                case 39:
+                case 68:
+                    scene[meshCtrld].rotation.y -= 0.01;
+                    render(scene, camera);
+                    break;
+                case 40:
+                case 83:
+                    scene[meshCtrld].rotation.x -= 0.01;
+                    render(scene, camera);
+                    break;
+                case 37:
+                case 65:
+                    scene[meshCtrld].rotation.y += 0.01;
+                    render(scene, camera);
+                    break;
+                case 90:
+                    scene[meshCtrld].position.z -= 1;
+                    render(scene, camera);
+                    break;
+                case 88:
+                    scene[meshCtrld].position.z += 1;
+                    render(scene, camera);
+                    break;
+                default:
+                    break;
+                }
+            break;
+                
+        case false:
+            switch(e.keyCode){
+                case 38:
+                case 87:
+                    scene[meshCtrld].position.y -= 1;
+                    render(scene, camera);
+                    break;
+                case 39:
+                case 68:
+                    scene[meshCtrld].position.x += 1;
+                    render(scene, camera);
+                    break;
+                case 40:
+                case 83:
+                    scene[meshCtrld].position.y += 1;
+                    render(scene, camera);
+                    break;
+                case 37:
+                case 65:
+                    scene[meshCtrld].position.x -= 1;
+                    render(scene, camera);
+                    break;
+                case 90:
+                    scene[meshCtrld].position.z -= 1;
+                    render(scene, camera);
+                    break;
+                case 88:
+                    scene[meshCtrld].position.z += 1;
+                    render(scene, camera);
+                    break;
+                }
+            break;
+    } 
 }
