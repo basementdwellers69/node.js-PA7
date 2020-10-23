11eb474161ff8907cd393cde469de9b659a0767f
basementdwellers69 <68178459+basementdwellers69@users.noreply.github.com>
2020-10-24 04:10:37 +0800

    v.0.1.3 - render.js Update

--- v0.0.3-1/render.js
+++ v0.0.3-1/render.js
@@ -1,3 +1,7 @@
+import {Vec} from './mesh.js';
+import {backfaceCullingIsTrue as backfaceCulling} from './UiControl.js';
+
+let camera = new Vec(0,0,100);
 function drawMesh(mesh, camera, context) {
     mesh.polygons.forEach(polygon => {
         if(polygon.color!= null){
@@ -19,6 +23,9 @@ function drawMesh(mesh, camera, context) {
 }
 
 function drawPolygon(polygon, context) {
+    if ((VecDotProduct(getNormal(polygon), camera)) > 0 && backfaceCulling === true){
+        return;
+    } ;
     polygon.forEach(point => {
         offsetToCenter(point, context.canvas);
     });
@@ -35,7 +42,30 @@ function drawPolygon(polygon, context) {
     context.stroke();
 }
 
-function offsetToCenter(point, canvas) {
+function vecCrossProduct(vecA, vecB){
+    let res = new Vec();
+    res.x = (vecA.x * vecB.z) - (vecA.z * vecB.y);
+    res.y = (vecA.z * vecB.x) - (vecA.x * vecB.z);
+    res.z = (vecA.x * vecB.y) - (vecA.y * vecB.x);
+    return res;
+}
+function VecDotProduct(VecA, VecB){
+    return (VecA.x * VecB.x) + (VecA.y * VecB.y) + (VecA.z * VecB.z);
+}
+
+function VecminVec(vecA, vecB){
+    let res = new Vec();
+    res.x = vecA.x - vecB.x;
+    res.y = vecA.y - vecB.y;
+    res.z = vecA.z - vecB.z;
+    return res;
+}
+
+function getNormal(polygon){
+    return vecCrossProduct((VecminVec(polygon[1], polygon[0])), (VecminVec(polygon[2], polygon[0])));
+}
+
+export function offsetToCenter(point, canvas) {
     point.x += canvas.width / 2;
     point.y += canvas.height / 2;
 }
