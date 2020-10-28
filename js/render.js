e20e6dd4808c50ebebdca92360bcbae2d8c58ca4
basementdwellers69 <68178459+basementdwellers69@users.noreply.github.com>
2020-10-29 02:22:41 +0800

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,3 +1,16 @@
+import {Vec} from './mesh.js';
+let backfaceCulling = false;
+let camera = new Vec(0,0,100);
+
+/// BACKFACE CULLING
+c17.addEventListener('click', function(){
+    if(this.value === "0"){
+        backfaceCulling = true;
+        this.value = "1";
+    }else{
+       backfaceCulling = false;
+    }
+});
 function drawMesh(mesh, camera, context) {
     context.strokeStyle = mesh.color;
 
@@ -14,6 +27,9 @@ function drawMesh(mesh, camera, context) {
 }
 
 function drawPolygon(polygon, context) {
+    if ((VecDotProduct(getNormal(polygon), camera)) > 0 && backfaceCulling === true){
+        return;
+    }
     polygon.forEach(point => {
         offsetToCenter(point, context.canvas);
     });
@@ -28,6 +44,29 @@ function drawPolygon(polygon, context) {
     context.lineTo(first.x, first.y);
 
     context.stroke();
+}
+
+function vecCrossProduct(vecA, vecB){
+    let res = new Vec();
+    res.x = (vecA.x * vecB.z) - (vecA.z * vecB.y);
+    res.y = (vecA.z * vecB.x) - (vecA.x * vecB.z);
+    res.z = (vecA.x * vecB.y) - (vecA.y * vecB.x);
+    return res;
+}
+function VecDotProduct(VecA, VecB){
+    return (VecA.x * VecB.x) + (VecA.y * VecB.y) + (VecA.z * VecB.z);
+}
+
+function VecminVec(vecA, vecB){
+    let res = new Vec();
+    res.x = vecA.x - vecB.x;
+    res.y = vecA.y - vecB.y;
+    res.z = vecA.z - vecB.z;
+    return res;
+}
+
+function getNormal(polygon){
+    return vecCrossProduct((VecminVec(polygon[1], polygon[0])), (VecminVec(polygon[2], polygon[0])));
 }
 
 function offsetToCenter(point, canvas) {
