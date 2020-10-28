138c5fa2871377e7965e4b3f52fcf4e16af6c374
basementdwellers69 <68178459+basementdwellers69@users.noreply.github.com>
2020-10-28 20:59:33 +0800

    v.0.2.0 - main.js update

--- v0.0.3-1/main.js
+++ v0.0.3-1/main.js
@@ -1,22 +1,21 @@
 import {cube, pyramid} from './models.js';
-import {createMesh, Vec} from './mesh.js';
+import {createMesh, Vec, offset, rotate} from './mesh.js';
 import {createWireframeRenderer} from './render.js';
-import {addli, resetList, addListAfterClick, input, polyListButton, ul, list, trash, press, fileHandler, backfaceCullingButton, setBackfaceCulling} from './UiControl.js';
+import {addli, resetList, addListAfterClick, input, polyListButton, ul, list, trash, press, fileHandler, backfaceCullingButton, setBackfaceCulling, saveMeshObj} from './UiControl.js';
 
 
 const canvas = document.querySelector('canvas');
 const canvasWindow = document.querySelector('#canvas');
 canvas.width = canvasWindow.clientWidth;
-console.log(canvas.width);
 canvas.height = canvasWindow.clientHeight;
 
-const mesh1 = createMesh(cube);
-mesh1.color = '#000';
+//const mesh1 = createMesh(cube);
+//mesh1.color = '#000';
 
-export const scene = [mesh1];
+export const scene = new Array();
 export let meshCtrld = 0;
 
-
+console.log(scene[meshCtrld]);
 
 ///FILE INPUT HANDLER
 window.addEventListener('load',fileHandler);
@@ -64,13 +63,14 @@ function animate(time) {
     requestAnimationFrame(animate);
 }
 
-scene[meshCtrld].polygons.map(addli);
+(scene[meshCtrld] === undefined || scene[meshCtrld].length < 0) ? console.log("no mesh loaded!") : scene[meshCtrld].polygons.map(addli) ;
 
 
 ///CHOOSE MESH BUTTON CONTROLS
 export const chooseMeshButton = document.querySelector('button');
 chooseMeshButton.onclick = function(){
-    
+    const colorInput = document.querySelector('#colorInput');
+    console.log(colorInput);
     let limit = scene.length-1;
     let value = parseInt(chooseMeshButton.value, 10);
     value = isNaN(value) ? 0 : value;
@@ -80,15 +80,33 @@ chooseMeshButton.onclick = function(){
         chooseMeshButton.value = value;
         chooseMeshButton.innerHTML = value;
         meshCtrld = value;
+        colorInput.value = scene[meshCtrld].color;
         resetList();
     }else{
         value = 0;
         chooseMeshButton.value = value;
         chooseMeshButton.innerHTML = 0;
         meshCtrld = 0;
+        colorInput.value = scene[meshCtrld].color;
         resetList();
     }
 }
+
+///  SAVE BUTTON HANDLER
+document.querySelector('#saveMesh').addEventListener("click", saveMeshObj);
+
+/// DEL MESH BUTTON HANDLER
+document.querySelector('#delMesh').addEventListener("click", function(){
+    scene.splice(meshCtrld, 1);
+    meshCtrld = 0;
+    render(scene, camera);
+});
+
+/// CHANGE MESH COLOR HANDLER
+document.querySelector('#color').addEventListener("click", function(){
+    scene[meshCtrld].color = colorInput.value;
+    render(scene, camera);
+})
 
 /// BACKFACE CULLING TOGGLE
 backfaceCullingButton.addEventListener("click", setBackfaceCulling);
