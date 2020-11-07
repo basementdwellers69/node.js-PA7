32ca6392d2ffa5dca80552f1dae806f2554a8dcf
Gerald <geraldo.febrian12@gmail.com>
2020-11-07 23:29:37 +0700

    fin

--- js/main.js
+++ js/main.js
@@ -1,37 +1,14 @@
-import {createMesh, Vec, rotate, offset} from './mesh.js';
+import {createMesh, Vec, rotate, offset, Camera} from './mesh.js';
 import {createWireframeRenderer, VecDotProduct, getNormal} from './render.js';
 
 scene = new Array()
 meshCtrld = 0
 sortedList=[]
 
-class Camera {
-    constructor() {
-        this.pos = new Vec(0, 0, 100);
-        this.zoom = 8;
-    }
-
-    project(point) {
-        perspective(point, this.pos.z);
-        zoom(point, this.zoom);
-    }
-}
-
-function perspective(point, distance) {
-    const fov = point.z + distance;
-    point.x /= fov;
-    point.y /= fov;
-}
-
-function zoom(point, factor) {
-    const scale = Math.pow(factor, 2);
-    point.x *= scale;
-    point.y *= scale;
-}
-
 camera = new Camera();
 camera.pos.z = 300;
 camera.zoom = 20;
+
 var adjustCanvasSize = function() {
     myCanvas.width = window.innerWidth * (65/100)
     myCanvas.height = window.innerHeight * (89/100)
